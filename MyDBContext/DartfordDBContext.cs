c7189a4e0aedc102546955dc8bc4310fd6758185
Asif Raza <asif.raza1013@gmail.com>
2025-06-26 16:45:17 +0500

    Update DartfordDBContext.cs

--- MyDBContext/DartfordDBContext.cs
+++ MyDBContext/DartfordDBContext.cs
@@ -1,15 +1,17 @@
-﻿using dartford_api.Models;
+using dartford_api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace dartford_api.MyDBContext
 {
     public class DartfordDBContext : DbContext
     {
+        public DartfordDBContext(DbContextOptions<DartfordDBContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Influencer> Influencers { get; set; }
         public DbSet<Plan> Plans { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=dpg-d1e5gi7diees73bgvp6g-a;Database=dartford;Username=root;Password=bPyGMMHTWQ457GcGFYHbJRUSTA2Mgw4M");
-
     }
 }

a9364706f54e1deac3a0c106795179f53bcd9762
Muhammad-Habban <mhabban2004@gmail.com>
2025-07-01 20:26:28 +0500

    temp

--- obj/Debug/net8.0/dartford_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/dartford_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dartford_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd340542693bf227850e3fa58c17b0cd8e676bad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8204d7836c456e3c4ecd5dd303b16ad2f4ac53c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dartford_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dartford_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
